17e0ea034dae76f9801f9bd081a0ef9187b8e14e
kanbin <kanbin@live.com>
2016-10-28 13:57:10 +0800

    tween动画

--- app/src/main/java/com/kb/frameanimationdemo/MainActivity.java
+++ app/src/main/java/com/kb/frameanimationdemo/MainActivity.java
@@ -1,24 +1,28 @@
 package com.kb.frameanimationdemo;
 
 import android.graphics.drawable.AnimationDrawable;
-import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageView mImageView;
     private AnimationDrawable mAnim;
+    private ImageView mImageView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mImageView = (ImageView) findViewById(R.id.imageView);
+        mImageView = (ImageView) findViewById(R.id.iv1);
         mAnim = (AnimationDrawable) mImageView.getBackground();
+
+        mImageView2 = (ImageView) findViewById(R.id.iv2);
     }
 
     public void start(View view) {
@@ -27,5 +31,10 @@ public class MainActivity extends AppCompatActivity {
 
     public void stop(View view) {
         mAnim.stop();
+    }
+
+    public void tween(View view) {
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.anim_tween);
+        mImageView2.startAnimation(animation);
     }
 }
